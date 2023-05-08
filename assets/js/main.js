7740b34b7da3d3ba42ca5a698570ddf25a91819d
Veinix <work.davidaviles@gmail.com>
2023-05-08 23:35:38 +0300

    Fix CSS, JS. New Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
-function disappear() {
-    document.getElementById("splashScreen").setAttribute("class", "makeDisappear");
+function fadeInClick() {
+    let section = document.querySelector("section");
+    section.setAttribute("class", "fadeIn_clickable");
+    setTimeout(function() {
+        section.removeAttribute("class", "fadeIn_clickable");
+    }, 2000);
 }
 
-function firstHome() {
-    const homepage = document.getElementById("home");
-    homepage.setAttribute("class", "fadeUp");
-    setTimeout(()=>homepage.removeAttribute("class", "fadeUp"), 3000);
+// Show splash only on first load
+function showSplash() {
+    let splash = document.querySelector("#splashScreen");
+    let currentUrl = window.location.href;
+    const desiredUrl = "http://127.0.0.1:5500/";
+
+    if (currentUrl.endsWith(desiredUrl)) {
+        splash.style.display = "block";
+    }
 }
+function hideSplash() {
+    let splash = document.querySelector("#splashScreen");
+    splash.style.display="none";
+    document.body.removeAttribute("onload");
+}
+
+
+
 
